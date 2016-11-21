b52e388b170ffd35f8632ee1b13f8f6e6eb90ff1
Michael Koontz <mikoontz@gmail.com>
2016-11-20 17:26:29 -0800

    determine that introduction gap not important for predicting establishment probability at F5 generation

--- scripts/extinction-probability-fixed-timepoint.R
+++ scripts/extinction-probability-fixed-timepoint.R
@@ -9,13 +9,15 @@
 ###
 #### Purpose: Use tidy extinction data to assess the probability of extinction depending on different covariates. ####
 ###
-### There are 3 different analysis presented here. 
+### There are 4 different analysis presented here. 
 
 ### The first uses generation 9 (the end of the experiment) as the fixed point where we assess whether or not a population has established.
 
 ### The second analysis uses generation 6 as the fixed point where we assess whether or not a population has established. Generation 6 is after all introductions have completed, but before we see the rapid decline in population abundance that may indicate an additional biological process is at work.
 
-### The third analysis uses all generations after introductions are complete (5 through 9) and includes a random effect of population ID to account for the repeated measures.
+### The third analysis uses all generations after introductions are complete (5 through 9) and includes a random effect of population ID to account for the repeated measures. This analysis was halted early on due to an inability to fit the models to the data.
+
+### The fourth analysis uses generation 5 as the fixed point where we assess whether or not a population has established. Generation 5 is the first generation after all introductions have completed for all populations.
 
 #### Load libraries, read data, and set it up for analysis ####
 # Clear environment if necessary
@@ -337,6 +339,64 @@ head(b_trim)
 
 # Even the simplest possible model that we'd be interested in won't fit using this framework. Let's abandon it in favor of fitting models for each generation that we care about.
 fm1 <- glmer(extant ~ number * environment + generation + (1 | ID) + (1 | block), data = b_trim, family = "binomial", control=glmerControl(optimizer="bobyqa"))
+
+##### Analysis 4: Extant/extinct assessment at generation 5 #####
+
+#### Analysis 4: Assessing effect of the introduction gap ####
+# Subset data to represent the only blocks and introduction treatments that COULD have experienced introduction gaps
+gap.potential <- subset(b, subset=(block!=3)&(number%in%c(2,4)))
+
+# We use a simple random effects structure here, because there is far less data in this subsetted dataset. Use extant assessment at generation 10 (9 full censuses after initial introduction).
+m1 <- glmer(extant5 ~ number*environment*gap + (1 | block), data=gap.potential, family=binomial, control=glmerControl(optimizer="bobyqa"))
+
+# Update the model fit to remove the 3-way interaction
+m2 <- update(m1, formula= .~. - number:environment:gap)
+
+anova(m1,m2) # LRT suggests model with 3-way interaction is not more likely than model with additive effect of gap. Also, model with 3-way interaction failed to converge. Drop 3-way interaction with gap.
+
+m3 <- update(m2, formula= .~. -gap:environment)
+
+anova(m2, m3) # LRT suggests model with interaction between gap and environment is not more likely than model without, so we drop the gap:environment interaction.
+
+m4 <- update(m3, formula= .~. -gap:number)
+
+anova(m3, m4) # LRT suggests model with interaction between gap and propagule number is not more likely than model without, so we drop the gap:number interaction.
+
+m5 <- update(m4, formula= .~. -gap)
+
+anova(m4, m5) # LRT suggests that the model with an additive effect of gap is NOT more likely than the model without, so we drop the effect of gap.
+
+#### Analysis 4: Plot effect (if any) of introduction gap ####
+# Quick plot to show marginal effect of introduction gap (very overlapping CIs when looking at generation 9)
+#--------------------
+# Recall that plogis() has the effect of being an inverse logit function and qlogis() has the effect of being a logit function.
+
+ls <- summary(lsmeans::lsmeans(m4, pairwise ~ as.factor(gap), adjust="none")$lsmeans)
+
+cont <- lsmeans::lsmeans(m4, pairwise ~ as.factor(gap), adjust="none")$contrasts
+cont.mu <- plogis(summary(cont)$estimate)
+cont.SE <- c(plogis(summary(cont)$estimate - summary(cont)$SE), plogis(summary(cont)$estimate + summary(cont)$SE))
+cont.SE
+
+mu <- plogis(ls$lsmean)
+CI <- rbind(plogis(ls$asymp.LCL), plogis(ls$asymp.UCL))
+
+# pdf("Clean Plots/introduction gap effect.pdf", height=3.75, width=3.75)
+par(mar=c(5,4,2,1))
+plot(x=1:2, y=mu, ylim=c(0.45, 1), xlim=c(0.5,2.5), las=1, pch=19, xaxt="n", xlab=NA, ylab="Probability of Establishment")
+axis(side=1, at=1:4, labels=rep(c("No gap", "Gap"), times = 2), tick=FALSE)
+
+arrows(x0=1:4, y0=CI[1,], y1=CI[2,], code=3, length=0.1, angle=90, lwd=2)
+# dev.off()
+
+mtext(side=3, text="Effect of introduction gap on establishment probability\nafter 5 generations")
+
+#-----------------------
+# No need to trim the data to exclude those populations that experienced an introduction gap.
+#-----------------------
+
+# Subset to remove populations with an introduction gap
+b.trim <- b
 
 
 #### Plots with simulation results ####
