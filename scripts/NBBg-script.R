8273b14c6668b205f40de12ad00a6fb19d6f0336
Michael Koontz <mikoontz@gmail.com>
2016-11-22 18:03:09 -0800

    Run simplified MCMC code to fit NBBg model to experimental data.

--- scripts/NBBg-script.R
+++ scripts/NBBg-script.R
@@ -26,7 +26,7 @@ data$residents <- 0
 data$Nt <- data$migrants + data$residents
 
 # Setup MCMC parameters
-n.mcmc <- 1000
+n.mcmc <- 10000
 inits <- c(R0=2, kE=22, kD=10, alpha=0.005)
 priors.shape <- c(R0=2.6, kE=17.6, kD=1.07, alpha=0.0037)
 priors.scale <- c(R0=1, kE=1, kD=1, alpha=1)
@@ -39,14 +39,29 @@ mcmc <- NBBG.mcmc(data, priors.shape, priors.scale, inits, tune, n.mcmc)
 burned <- burn.in(mcmc[c("R0", "kE", "kD", "alpha")], 2000)
 burned.df <- burn.in.df(mcmc[c("RE", "F.migrants", "F.residents")], 2000)
 
-# mcmc[["accept"]]
+# Percent of proposals accepted
+(mcmc[["accept"]] / n.mcmc) * 100
+
+R0 <- mean(burned[["R0"]])
+alpha <- mean(burned[["alpha"]])
+x <- 1:200
+mu_Ntp1 <- x * R0 * exp(-alpha * x)
+
+par(mfrow=c(3,2))
+plot(data$Nt, data$Ntplus1, pch=19)
+lines(x = x, y = mu_Ntp1, col="red")
+plot(burned[["R0"]], type="l", main="R0")
+plot(burned[["kE"]], type="l", main="kE")
+plot(burned[["kD"]], type="l", main="kD")
+plot(burned[["alpha"]], type="l", main=expression(alpha))
+plot(burned.df[["RE"]][2, ], type="l", main="RE")
+
 
 #### Write the samples data file for my data ####
 # samples <- data.frame(R0=burned[["R0"]], kE=burned[["kE"]], kD=burned[["kD"]], alpha=burned[["alpha"]])
 # write.csv(samples, 'NBBg samples.csv', row.names=FALSE)
 
 #### Trace plots and a sample trace plot for RE (1st population) ####
-
 
 # pdf('test.pdf')
 # par(mfrow=c(3,2))
@@ -91,220 +106,6 @@ burned.df <- burn.in.df(mcmc[c("RE", "F.migrants", "F.residents")], 2000)
 # 3        kD  1.089430833  0.732032613  0.407613921  2.816244763
 # 4     alpha  0.003717655  0.003707539  0.003398071  0.004049347
 # 5        RE  2.602043286  2.457251521  1.644039932  3.887305169
-
-#### Simulated data to validate the MCMC algorithm ####
-### Simulate data
-
-# df <- data.frame(residents=rep(0,2600), migrants=c(rep(4,500), rep(5, 500), rep(10, 500), rep(20, 500), rep(50, 200), rep(100,200), rep(200, 100), rep(1000, 100)) )
-# df <- data.frame(residents=rep(0,250), migrants=c(rep(seq(2,100), times=2), rep(seq(125, 500, by=25), times=2), rep(seq(550, 1000, by=50), times=2)) )
-# 
-# R0 <- 2.6
-# kE <- 17.6
-# kD <- 1.07
-# alpha <- 0.0037
-# 
-# test <- Tribolium.NBBG(data=df, R0=R0, kE=kE, kD=kD, alpha=alpha)
-# x <- 1:1000
-# 
-# # par(mfrow=c(3,2))
-# plot(test$Nt, test$Ntplus1, pch=".")
-# lines(x=x, y=x * R0 * exp(-alpha*x), col="red")
-# head(test)
-
-# priors.shape <- c(R0=2.6, kE=17.6, kD=1.07, alpha=0.0037)
-# priors.scale <- c(R0=1, kE=1, kD=1, alpha=1)
-# tune <- c(R0=0.2, kE=5, kD=0.75, alpha=0.0001, RE=1.5)
-# 
-# mcmc.test <- NBBG.mcmc(test, priors.shape, priors.scale, inits, tune, n.mcmc)
-# mcmc <- mcmc.test
-# mcmc[["accept"]]
-
-
-#### Data from Melbourne & Hastings (2008) ####
-
-# melbourne <- read.csv("melbourne_ricker_data.csv")
-# head(melbourne)
-# melbourne$At <- round(melbourne$At)
-# melbourne$Atp1 <- round(melbourne$Atp1)
-# melbourne$residents <- 0
-# melbourne$migrants <- melbourne$Nt
-# 
-# ### Define MCMC arguments
-# 
-# n.mcmc <- 5000
-# inits <- c(R0=2, kE=22, kD=10, alpha=0.005)
-
-# colnames(melbourne)[colnames(melbourne)=="Atp1"] <- "Ntplus1"
-# colnames(melbourne)[colnames(melbourne)=="At"] <- "migrants"
-# melbourne$residents <- 0
-# melbourne$Nt <- melbourne$migrants + melbourne$residents
-# melbourne
-# plot(melbourne$Nt, melbourne$Ntplus1, pch=19)
-#--------------
-# For Brett's data
-# priors.shape <- c(R0=0.001, kE=0.001, kD=0.001, alpha=0.001)
-# priors.scale <- c(R0=1000, kE=1000, kD=1000, alpha=1000)
-# tune <- c(R0=0.2, kE=15, kD=1, alpha=0.0002, RE=1.2)
-# mcmc.melbourne <- NBBG.mcmc(melbourne, priors.shape, priors.scale, inits, tune, n.mcmc)
-# mcmc.melbourne[["accept"]]
-
-# par(mfrow=c(3,2))
-# plot(melbourne$Nt, melbourne$Ntplus1, pch=19)
-# lines(x=x, y=x * results[results$parameter=="R0", "mean"] * exp(-results[results$parameter=="alpha", "mean"]*x), col="red")
-# plot(mcmc.melbourne[["R0"]], type="l", main="R0")
-# plot(mcmc.melbourne[["kE"]], type="l", main="kE")
-# plot(mcmc.melbourne[["kD"]], type="l", main="kD")
-# plot(mcmc.melbourne[["alpha"]], type="l", main=expression(alpha))
-# plot(mcmc.melbourne[["RE"]][1, ], type="l", main="RE")
-# burned <- burn.in(mcmc.melbourne[c("R0", "kE", "kD", "alpha")], 500)
-# burned.df <- burn.in.df(mcmc.melbourne[c("RE", "F.migrants", "F.residents")], 150)
-# dev.off()
-
-#### Write the samples file for Brett's data ####
-# samples <- data.frame(R0=burned[["R0"]], kE=burned[["kE"]], kD=burned[["kD"]], alpha=burned[["alpha"]])
-# 
-# write.csv(samples, 'NBBg samples melbourne.csv', row.names=FALSE)
-
-#### Testing environmental stochasticity effect ####
-
-# We expect the fluctuating environment to have a greater value for kE than the stable environment
-
-# d <- read.csv("Census data through generation 10.csv")
-# d <- subset(d, subset=Generation==1)
-# d <- subset(d, select=c(ID, Stochastic, N0, Census))
-# colnames(d)[2:4] <- c("env", "migrants", "Ntplus1")
-# d$residents <- 0
-# d$Nt <- d$residents + d$migrants
-# head(d)
-# 
-# fluc <- subset(d, subset=env=="fluctuating")
-# stab <- subset(d, subset=env=="stable")
-# 
-# n.mcmc <- 10000
-# inits <- c(R0=2, kE=22, kD=10, alpha=0.005)
-# priors.shape <- c(R0=2.6, kE=17.6, kD=1.07, alpha=0.0037)
-# priors.scale <- c(R0=1, kE=1, kD=1, alpha=1)
-
-# fluc.tune <- c(R0=0.08, kE=8, kD=1, alpha=0.008, RE=1)
-# stab.tune <- c(R0=0.08, kE=8, kD=1, alpha=0.008, RE=1)
-# 
-# mcmc.fluc <- NBBG.mcmc(fluc, priors.shape, priors.scale, inits, fluc.tune, n.mcmc)
-# mcmc.stab <- NBBG.mcmc(stab, priors.shape, priors.scale, inits, stab.tune, n.mcmc)
-# 
-# mcmc.fluc[["accept"]]
-# mcmc.stab[["accept"]]
-
-# Fluctuating environment first
-# x <- 1:20
-# par(mfrow=c(3,2))
-# plot(fluc$Nt, fluc$Ntplus1, pch=19)
-# # lines(x=x, y=x * R0 * exp(-alpha*x), col="red")
-# plot(mcmc.fluc[["R0"]], type="l", main="R0")
-# plot(mcmc.fluc[["kE"]], type="l", main="kE")
-# plot(mcmc.fluc[["kD"]], type="l", main="kD")
-# plot(mcmc.fluc[["alpha"]], type="l", main=expression(alpha))
-# plot(mcmc.fluc[["RE"]][2, ], type="l", main="RE")
-# burned.fluc <- burn.in(mcmc.fluc[c("R0", "kE", "kD", "alpha")], 2000)
-# burned.df.fluc <- burn.in.df(mcmc.fluc[c("RE", "F.migrants", "F.residents")], 2000)
-# 
-# plot(stab$Nt, stab$Ntplus1, pch=19)
-# plot(mcmc.stab[["R0"]], type="l", main="R0")
-# plot(mcmc.stab[["kE"]], type="l", main="kE")
-# plot(mcmc.stab[["kD"]], type="l", main="kD")
-# plot(mcmc.stab[["alpha"]], type="l", main=expression(alpha))
-# plot(mcmc.stab[["RE"]][2, ], type="l", main="RE")
-# burned.stab <- burn.in(mcmc.stab[c("R0", "kE", "kD", "alpha")], 2000)
-# burned.df.stab <- burn.in.df(mcmc.stab[c("RE", "F.migrants", "F.residents")], 2000)
-# 
-# stack <- c(density(burned.fluc[["kE"]])$y, density(burned.stab[["kE"]])$y)
-# max.y.idx <- which.max(stack)
-# max.y <- stack[max.y.idx]
-# 
-# par(mfrow=c(1,1))
-# plot(density(burned.fluc[["kE"]]), lwd=2, col="red", main="Parameter estimate for environmental stochasticity", ylim=c(0,max.y))
-# lines(density(burned.stab[["kE"]]), lwd=2, col="blue")
-# legend("topright", legend=c("Stable", "Fluctuating"), col=c("blue", "red"), lwd=3)
-
-# mu is R0
-# var is R0^2 / kE
-
-# fRE.mu <- burned.fluc[["R0"]]
-# fRE.s2 <- fRE.mu^2 / burned.fluc[["kE"]]
-# 
-# sRE.mu <- burned.stab[["R0"]]
-# sRE.s2 <- sRE.mu^2 / burned.stab[["kE"]]
-#   
-# t.test(burned.fluc[["kE"]], burned.stab[["kE"]])
-# 
-# stack <- c(density(burned.fluc[["R0"]])$y, density(burned.stab[["R0"]])$y)
-# max.y.idx <- which.max(stack)
-# max.y <- stack[max.y.idx]
-# 
-# par(mfrow=c(1,1))
-# plot(density(burned.stab[["R0"]]), lwd=2, col="blue", main="Parameter estimate for R0", ylim=c(0,max.y))
-# lines(density(burned.fluc[["R0"]]), lwd=2, col="red")
-# legend("topright", legend=c("Stable", "Fluctuating"), col=c("blue", "red"), lwd=3)
-# 
-# stack <- c(density(sRE.s2)$y, density(fRE.s2)$y)
-# max.y.idx <- which.max(stack)
-# max.y <- stack[max.y.idx]
-# 
-# par(mfrow=c(1,1))
-# plot(density(sRE.s2), lwd=2, col="blue", main="Estimate for derived parameter: variance of RE", ylim=c(0,max.y))
-# lines(density(fRE.s2), lwd=2, col="red")
-# legend("topright", legend=c("Stable", "Fluctuating"), col=c("blue", "red"), lwd=3)
-
-
-
-# 
-# 
-# 
-
-#### Trace plots after removing burn in ####
-
-# par(mfrow=c(3,2))
-# plot(test$Nt, test$Ntplus1, pch=19)
-# lines(x=x, y=x * R0 * exp(-alpha*x), col="red")
-# plot(burned[["R0"]], type="l", main="R0")
-# plot(burned[["kE"]], type="l", main="kE")
-# plot(burned[["kD"]], type="l", main="kD")
-# plot(burned[["alpha"]], type="l", main=expression(alpha))
-# plot(burned.df[["RE"]][1, ], type="l", main="RE")
-# 
-# length(burned.df)
-
-####  Plots of priors and posteriors ####
-# par(mfrow=c(3,2))
-# plot(density(burned[["R0"]]), main="R0")
-# # abline(v=R0, col="red")
-# R0.x <- seq(0, 12, 0.5)
-# R0.y <- dgamma(R0.x, shape=priors.shape["R0"], scale=priors.scale["R0"])
-# lines(R0.x, R0.y, col="blue")
-# 
-# plot(density(burned[["kE"]]), main="kE")
-# # abline(v=kE, col="red")
-# kE.x <- seq(5,35,0.5)
-# kE.y <- dgamma(kE.x, shape=priors.shape["kE"], scale=priors.scale["kE"])
-# lines(kE.x, kE.y, col="blue")
-# 
-# plot(density(burned[["kD"]]), main="kD")
-# # abline(v=kD, col="red")
-# kD.x <- seq(0,20,0.05)
-# kD.y <- dgamma(kD.x, shape=priors.shape["kD"], scale=priors.scale["kD"])
-# lines(kD.x, kD.y, col="blue")
-# 
-# plot(density(burned[["alpha"]]), main=expression(alpha))
-# # abline(v=alpha, col="red")
-# alpha.x <- seq(0, 0.4, 0.001)
-# alpha.y <- dgamma(alpha.x, shape=priors.shape["alpha"], scale=priors.scale["alpha"])
-# lines(alpha.x, alpha.y, col="blue")
-
-# RE.x <- seq(0, 20, by=0.05)
-# RE.y <- dgamma(RE.x, shape=kE, scale=R0/kE)
-# plot(RE.x, RE.y, col="red", main="RE", type="l")
-# lines(density(burned.df[["RE"]]))
-# plot(density(burned.df[["RE"]]))
-
 
 
 
