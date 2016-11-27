11cd66b3a8af63be11848e3ca681f648ce47890b
Michael Koontz <mikoontz@gmail.com>
2016-11-27 14:37:23 -0800

    Make all calls to external references relative within repo. Visualization of difference by treatment.

--- scripts/environmental-stochasticity.R
+++ scripts/environmental-stochasticity.R
@@ -3,24 +3,23 @@
 ### Author: Michael Koontz
 ###
 ### Date Created: 20140715
-### Last Updated: 20160113
-###
+### Penultimate Update: 20160113
+### Last Updated: 20161127 (all further updates reflected in version history)
+
 ### Purpose: Determine whether fluctuating environment treatment corresponded to a greater var(log(lambda))
 
-rm(list=ls())
+# Clear environment if necessary
+# rm(list=ls())
 
-if (!require("lme4"))
-{install.packages("lme4"); library(lme4)}
-if (!require("lsmeans"))
-{install.packages("lsmeans"); library(lsmeans)}
+library(lme4)
+library(lsmeans)
 
-setwd("/Users/mikoontz/Documents/Research/Tribolium/Demography")
-source("Clean Data/generate-tidy-data.R")
+source("scripts/data-carpentry/generate-tidy-data.R")
 
-beetles <- read.csv("Clean-Data/Tribolium-propagule-pressure-data.csv")
-attributes <- read.csv("Clean-Data/attributes.csv")
+beetles <- read.csv("data/Tribolium-propagule-pressure-data.csv")
+attributes <- read.csv("data/attributes.csv")
 
-b <- tidy.beetles(beetles=beetles) # Convert long form data to short form (one generation per column)
+b <- tidy.beetles(beetles = beetles) # Convert long form data to short form (one generation per column)
 data_col <- 2:(ncol(b$Nt) - 1) # The data columns (i.e. all time steps, but not including ID column for the tidy data.frames)
 
 lambdas <- b$Ntp1[, data_col] / b$Nt[, data_col] # Calculate year to year population growth rates
@@ -29,13 +28,19 @@ stoch_tot <- data.frame(attributes, stoch_tot=apply(lambdas, 1, function(x) var(
 #------------
 # Fitted models testing for effect of environment group (stable vs fluctuating) on actual total stochasticity
 #------------
+# Models are fit using only populations that never went temporarily or permanently extinct (n=667)
 
-fm1 <- lmer(stoch_tot ~ environment + (1 + environment | block), data=stoch_tot)
+fm1 <- lmer(stoch_tot ~ environment + (environment | block), data=stoch_tot)
 fm2 <- lmer(stoch_tot ~ environment + (1 | block), data=stoch_tot)
 
-anova(fm1, fm2) # Drop environment * block interaction
+anova(fm1, fm2) # Drop random slope effect of environment
 
 # Significantly different contrast in total stochasticity between stable and fluctuating environment (estimate=0.04707 p=0.0201)
-lsmeans::lsmeans(fm2, pairwise ~ environment)$contrasts
+results <- lsmeans::lsmeans(fm2, pairwise ~ environment)
+(contr <- results$contrasts)
 
-
+#### Plot density of total stochasticity for each environment treatment ####
+plot(density(stoch_tot$stoch_tot[stoch_tot$environment == "stable"], na.rm= TRUE))
+lines(density(stoch_tot$stoch_tot[stoch_tot$environment == "fluctuating"], na.rm= TRUE), col = "red")
+abline(v = summary(results)$lsmeans$lsmean[1], lwd = 2, col = "red")
+abline(v = summary(results)$lsmeans$lsmean[2], lwd = 2, col = "black")
