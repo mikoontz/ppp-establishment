f834b536a8918d635e305ffeff21d43acef1ef80
Michael Koontz <mikoontz@gmail.com>
2016-11-28 22:33:22 -0800

    setup for final simulation run with lots of replicates and writing all data to file.

--- scripts/simulations/NBBg-simulation-script.R
+++ scripts/simulations/NBBg-simulation-script.R
@@ -10,7 +10,7 @@ source("scripts/data-carpentry/generate-establishment-responses-for-analysis.R")
 
 # Samples from posterior distribution of R0, alpha, kE, and kD parameters
 samps <- read.csv('data/NBBg-samples/NBBg-samples-combined.csv')
-reps <- 10 # Number of reps per introduction scenario
+reps <- 500000 # Number of reps per introduction scenario
 Tf <- 10 # Number of time steps, INCLUDING initial introduction
 total.to.introduce <- 20
 p <- 0.5 # Probability of being female
@@ -46,7 +46,7 @@ extant_prop_after_x <- simulation_stats(time_points = time_points,
                                         N = N[, -1], # Just pass the equivalent of the Ntp1 dataframe (i.e. no initial introduction column)
                                         intro.regime = intro.regime,
                                         gap = rep(FALSE, nrow(pp) * reps),
-                                        gens.censused = rep(Tf, nrow(pp) * reps),
+                                        gens.censused = rep(Tf - 1, nrow(pp) * reps), # 1 fewer census than total time steps
                                         col_names = paste0("extant_prop_after_", time_points))
 #### Extant proportion at fixed time points for fluctuating environment ####
 # Population still present at generations 5, 6, 7, 8, and 9 for fluctuating environment treatment
@@ -55,8 +55,8 @@ extant_prop_after_x_var <- simulation_stats(time_points = time_points,
                                             N = N_var[, -1], # Just pass the equivalent of the Ntp1 dataframe (i.e. no initial introduction column)
                                             intro.regime = intro.regime,
                                             gap = rep(FALSE, nrow(pp) * reps),
-                                            gens.censused = rep(Tf, nrow(pp) * reps),
-                                            col_names = paste0("extant_prop_after_", time_points))
+                                            gens.censused = rep(Tf - 1, nrow(pp) * reps), # 1 fewer census than total time steps
+                                            col_names = paste0("extant_prop_after_", time_points, "_var"))
 #### Population sizes at fixed time points for stable populations ####
 # First, convert all Ntp1 censuses of 0 to NA so they don't get included in averages
 N_for_abundance <- N
@@ -73,7 +73,7 @@ N_for_abundance_var[, -1] <- sapply(N_for_abundance_var[, -1], function(j) repla
 mean_N_after_x_var <- apply(X = N_for_abundance_var[, time_points + 1], 
                         MARGIN = 2, 
                         FUN = function(x) tapply(x, INDEX = intro.regime, FUN = mean, na.rm = TRUE))
-colnames(mean_N_after_x_var) <- paste0("N_after", time_points)
+colnames(mean_N_after_x_var) <- paste0("N_after", time_points, "_var")
 
 ##### Establishment proportions and population abundance at relative time points #####
 # (x number of generations after final introduction event)#### Establshment proportions at relative time points
@@ -86,7 +86,7 @@ extant_prop_x_after <- simulation_stats(time_points = time_points,
                                         N = N[, -1], # Just pass the equivalent of the Ntp1 dataframe (i.e. no initial introduction column)
                                         intro.regime = intro.regime,
                                         gap = rep(FALSE, nrow(pp) * reps),
-                                        gens.censused = rep(Tf, nrow(pp) * reps),
+                                        gens.censused = rep(Tf - 1, nrow(pp) * reps), # 1 fewer census than total time steps
                                         col_names = paste0("extant_prop_", time_points, "after"))
 #### Establishment proportion at relative time points for fluctuating environment ####
 # Population still present 1, 2, 3, 4, and 5 generations after final introduction event for fluctuating environment treatment
@@ -95,8 +95,8 @@ extant_prop_x_after_var <- simulation_stats(time_points = time_points,
                                             N = N_var[, -1], # Just pass the equivalent of the Ntp1 dataframe (i.e. no initial introduction column)
                                             intro.regime = intro.regime,
                                             gap = rep(FALSE, nrow(pp) * reps),
-                                            gens.censused = rep(Tf, nrow(pp) * reps),
-                                            col_names = paste0("extant_prop_", time_points, "after"))
+                                            gens.censused = rep(Tf - 1, nrow(pp) * reps), # 1 fewer census than total time steps
+                                            col_names = paste0("extant_prop_", time_points, "after_var"))
 #### Population sizes at relative time points for stable populations ####
 mean_N_x_after <- simulation_stats(time_points = time_points,
                                    FUN = N_x.after.intro,
@@ -108,4 +108,20 @@ mean_N_x_after_var <- simulation_stats(time_points = time_points,
                                        FUN = N_x.after.intro,
                                        Ntp1 = N_var[, -1], 
                                        intro.regime = intro.regime,
-                                       col_names = paste0("N_", time_points, "after"))
+                                       col_names = paste0("N_", time_points, "after_var"))
+
+results <- data.frame(
+  extant_prop_after_x,
+  extant_prop_after_x_var,
+  mean_N_after_x,
+  mean_N_after_x_var,
+  extant_prop_x_after,
+  extant_prop_x_after_var,
+  mean_N_x_after,
+  mean_N_x_after_var)
+
+results
+
+write.csv(x = results, file = "data/simulations/simulation_stats.csv", row.names = FALSE)
+write.csv(x = N, file = "data/simulations/N.csv", row.names = FALSE)
+write.csv(x = N_var, file = "data/simulations/N_var.csv", row.names = FALSE)
