da642531926ae32f292286b4d08f04f74f91717a
Michael Koontz <mikoontz@gmail.com>
2017-01-27 11:42:48 -0800

    annotate, clean up analyses, remove unnecessary pieces.

--- scripts/measurement-error.R
+++ scripts/measurement-error.R
@@ -3,32 +3,41 @@
 ### Author: Michael Koontz
 ### 
 ### Date Created: 20140604
-### Last Updated: 20140604
+### Last Updated: 20170127
 ###
-### Purpose: Evaluates the measurement error in censusing using repeat measures of 15 populations of unknown size
+# Purpose: Evaluates the measurement error in censusing using repeat measures 
+# of 15 populations of unknown size
 
-setwd("/Users/mikoontz/Documents/Research/Tribolium/Demography")
-error <- read.csv("Entered Data/csv/Measurement Error/20140602 measurement error.csv")
+# Fifteen populations were set up with unknown, but variable population sizes 
+# and 8 different observers censused each population. Five populations were
+# "small", 5 populations were "medium", and 5 populations were "large"
 
+# Read file
+error <- read.csv("data/measurement-error.csv")
+error$size_cat <- c(rep("s", 5), rep("m", 5), rep("l", 5))
 head(error)
 
-par(mar=c(5,5,4,3)+0.1)
-plot(error$Box, error$Census, pch=19)
+# Plot the range of census values
+plot(x = rep(1, times = nrow(error)), y = error$Census, pch = 19)
+boxplot(error$Census, add = TRUE, col = NA)
 
-variance <- tapply(error$Census, error$Box, FUN=function(x) var(x, na.rm=TRUE))
+# Mean and variance of censused population sizes. If variance is 0, then all 
+# observers recorded the same population size 
+(group.mn <- tapply(error$Census, error$Box, FUN=function(x) mean(x, na.rm=TRUE)))
+(variance <- tapply(error$Census, error$Box, FUN=function(x) var(x, na.rm=TRUE)))
 
-group.mn <- tapply(error$Census, error$Box, FUN=function(x) mean(x, na.rm=TRUE))
-group.mn
+# The squared difference between each observer's census and the group mean for
+# each population
+(ss_resids <- tapply(X = error$Census, 
+                  INDEX = error$Person, 
+                  FUN=function(x) ( ((x - group.mn)^2) ) ))
 
-resids <- tapply(error$Census, error$Person, FUN=function(x) ( ((x-group.mn)^2) ) )
-resids
+ss_resids_df <- do.call(rbind, ss_resids)
 
-df <- do.call(rbind, resids)
-mean(apply(df, 2, function(x) (mean(x, na.rm=TRUE))^(1/2) ) / group.mn)
-apply(df, 1, function(x) sd(x/group.mn, na.rm=TRUE))
-
-lapply(resids, FUN=function(x) sum(x^2, na.rm=TRUE))
-
-var(variance[1:5])
-var(variance[6:10])
-var(variance[10:15])
+# Coefficient of variation of observer error per population (standard deviation of
+# sum of squares residuals per popoulation divided by group mean per population)
+CoV <- apply(ss_resids_df, MARGIN = 2, FUN = function(x) mean(sqrt(x), na.rm = TRUE)) / group.mn
+mean(CoV)
+  
+fm1 <- lm(Census ~ Person + size_cat, data = error)
+summary(fm1)
