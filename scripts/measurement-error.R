4c7a27b508c29d645e35ecfee706baa91c582887
Michael Koontz <mikoontz@gmail.com>
2017-01-27 13:43:49 -0800

    more rigorous quantification of average measurement error, observer-specific measurement error, and population size effect on measurement error.

--- scripts/measurement-error.R
+++ scripts/measurement-error.R
@@ -11,6 +11,8 @@
 # Fifteen populations were set up with unknown, but variable population sizes 
 # and 8 different observers censused each population. Five populations were
 # "small", 5 populations were "medium", and 5 populations were "large"
+library(dplyr)
+library(tidyr)
 
 # Read file
 error <- read.csv("data/measurement-error.csv")
@@ -34,10 +36,32 @@ boxplot(error$Census, add = TRUE, col = NA)
 
 ss_resids_df <- do.call(rbind, ss_resids)
 
-# Coefficient of variation of observer error per population (standard deviation of
-# sum of squares residuals per popoulation divided by group mean per population)
-CoV <- apply(ss_resids_df, MARGIN = 2, FUN = function(x) mean(sqrt(x), na.rm = TRUE)) / group.mn
-mean(CoV)
-  
-fm1 <- lm(Census ~ Person + size_cat, data = error)
+# Coefficient of variation of observer error per population (standard deviation
+# of sum of squares residuals per popoulation per observer divided by group mean
+# per population)
+CoV <- apply(ss_resids_df, MARGIN = 2, FUN = function(x) sqrt(x)) %>%
+  apply(MARGIN = 1, FUN = function(y) y / group.mn) %>%
+  t()
+
+pct_err_long <- CoV %>%
+  as.data.frame() %>%
+  mutate(observer = rownames(.)) %>%
+  gather(key = "box", value = "pct_err", -observer) %>%
+  mutate(size_cat = "s") %>%
+  mutate(size_cat = ifelse(box %in% 1:5, yes = "s", no = ifelse(box %in% 6:10, yes = "m", no = "l"))) %>%
+  mutate(group_mn = group.mn[box]) # Add the group means for each box
+
+# Treat the population size as a categorical predictor with 3 levels (small, medium, large)
+fm1 <- lm(pct_err ~ observer + size_cat, data = pct_err_long)
 summary(fm1)
+
+# Treat the group means as a continuous predictor
+fm2 <- lm(pct_err ~ observer + group_mn, data = pct_err_long)
+summary(fm2)
+
+# Conclusion: No difference in measurement error amongst observers, no effect of
+# population size on measurement error %
+
+# Average percent difference for any one observer and any one population was 0.26%
+fm3 <- lm(pct_err ~ 1, data = pct_err_long)
+summary(fm3)
