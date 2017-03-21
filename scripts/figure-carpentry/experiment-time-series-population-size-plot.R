b274e1cc294bedb4ccc053ac71292bd4c6c66e5b
Michael Koontz <mikoontz@gmail.com>
2017-03-21 12:41:48 -0700

    make time series summary calculations into a function

--- scripts/figure-carpentry/experiment-time-series-population-size-plot.R
+++ scripts/figure-carpentry/experiment-time-series-population-size-plot.R
@@ -50,31 +50,42 @@ data <- data[data$gap == FALSE, ]
 
 # We need to calculate the mean and standard error of population size by generation and introduction regime. So we need mean abundance, standard deviation of abundance, and sample size. We'll use calculations that exclude populations that have been extinct (represented as NAs in the data) and populations that JUST went extinct (represented as zeroes before becoming NAs in the next generation).
 
+ts_getSummary <- function(data, INDEX = data$number) {
+
 means_no_zeroes <- apply(data[, 2:20], 
                          MARGIN= 2, 
                          FUN= function(x) tapply(x, 
-                                                 INDEX= data$number, 
+                                                 INDEX= INDEX, 
                                                  FUN= function(y) mean(y[y>0],  na.rm= TRUE)))
 
 counts_no_zeroes <- apply(data[, 2:20], 
                           MARGIN= 2, 
                           FUN= function(x) tapply(x, 
-                                                  INDEX= data$number, 
+                                                  INDEX= INDEX, 
                                                   FUN= function(y) length(which(!is.na(y) & y > 0))))
 
 sigma_no_zeroes <- apply(data[, 2:20], 
                          MARGIN= 2, 
                          FUN= function(x) tapply(x, 
-                                                 INDEX= data$number, 
+                                                 INDEX= INDEX, 
                                                  FUN= function(y) sd(y, na.rm= TRUE)))
 
 ##### Calculate the standard error and time steps ####
-se_no_zeroes <- sigma_no_zeroes / sqrt(counts_no_zeroes)
+se_no_zeroes <- sigma_no_zeroes / sqrt(counts_no_zeroes - 1)
+
+return(list(means_no_zeroes = means_no_zeroes, se_no_zeroes = se_no_zeroes))
+}
+
+ts_summary <- ts_getSummary(data, data$number)
+se_no_zeroes <- ts_summary$se_no_zeroes
+means_no_zeroes <- ts_summary$means_no_zeroes
 
 time <- c(0, rep(1:9, each=2))
 
-#### Make time series plot through Generation 7 ####
-pdf("figures/experiment-time-series-population-size-seven-generations.pdf", height=5, width=6)
+#### Make a 2-panel plot of population size through generation 7 where each panel has a different environment treatment
+# pdf("figures/experiment-time-series-population-size-seven-generations-environment-facet.pdf", height=5, width=6)
+
+
 par(mar = c(4.7, 4.7, 1, 1), family = "Helvetica", mgp = c(3.3, 1, 0))
 
 matplot(x = time[1:15], y = t(time_series[, 2:16]), 
@@ -109,7 +120,45 @@ for (j in 1:nrow(means_no_zeroes))
 }
 matplot(time[1:15], t(means_no_zeroes[, 1:15]), add= TRUE, col=viridis(4), type="l", lwd=1.5, lty="solid")
 
-dev.off()
+# dev.off()
+
+#### Make time series plot through Generation 7 ####
+# pdf("figures/experiment-time-series-population-size-seven-generations.pdf", height=5, width=6)
+# par(mar = c(4.7, 4.7, 1, 1), family = "Helvetica", mgp = c(3.3, 1, 0))
+# 
+# matplot(x = time[1:15], y = t(time_series[, 2:16]), 
+#         type = "l", 
+#         lty = "solid", 
+#         col = adjustcolor("grey", alpha.f= 0.1), 
+#         las = 1, 
+#         xlab = "Generation", 
+#         ylab = "Population size", 
+#         xaxt = "n", 
+#         ylim = c(0,80),
+#         cex.lab = 1.5,
+#         cex.axis = 1.5)
+# 
+# legend("topleft", 
+#        legend = c("20 individuals introduced in 1 event", "10x2", "4x5", "5x4"), 
+#        bty = "n", 
+#        col = viridis(4), 
+#        lty = 1,
+#        lwd = 3)
+# 
+# axis(side = 1, 
+#      at = 0:7)
+# 
+# # ---------
+# # Plot a 1 standard error envelope around the mean population abundances
+# # ---------
+# for (j in 1:nrow(means_no_zeroes))
+# {
+#   
+#   polygon(x= c(time[1:15], time[15:1]), y= c((means_no_zeroes[j, 1:15] - se_no_zeroes[j, 1:15]), means_no_zeroes[j, 15:1] + se_no_zeroes[j, 15:1]), col=adjustcolor(viridis(4)[j], alpha.f= 0.5), border = NA)
+# }
+# matplot(time[1:15], t(means_no_zeroes[, 1:15]), add= TRUE, col=viridis(4), type="l", lwd=1.5, lty="solid")
+# 
+# dev.off()
 
 
 # #### Make time series plot through Generation 9 ####
